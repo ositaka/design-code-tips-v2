f2709cc90b5cbcb0726b4c7a7c9fdb1ea31f0598
Nuno Marques <ositaka@gmail.com>
2022-03-31 13:43:48 +0100

    test on deploy

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -4,7 +4,7 @@ import { func, string } from 'prop-types'
 
 const Toggle = ({ theme, toggleTheme }) => {
   const isLight = theme === 'light-theme'
-  return <button onClick={toggleTheme}>{isLight ? 'sun' : 'moon'}</button>
+  return <button onClick={toggleTheme} title="Click here to change the theme">{isLight ? 'sun' : 'moon'}</button>
 }
 
 Toggle.propTypes = {
