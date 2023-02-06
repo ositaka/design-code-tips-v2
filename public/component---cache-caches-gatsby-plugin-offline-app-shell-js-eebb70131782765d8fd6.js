180db7f4ff6047862ca67e2d0626ed7608e47d5d
Nuno Marques <ositaka@gmail.com>
2023-02-06 21:24:49 +0100

    Working locally

--- public/component---cache-caches-gatsby-plugin-offline-app-shell-js-eebb70131782765d8fd6.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdesign_code_tips_v2=self.webpackChunkdesign_code_tips_v2||[]).push([[306],{1367:function(e,t,n){var u=n(5318);t.__esModule=!0,t.default=void 0;var r=u(n(5354)),a=u(n(7294)),d=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},t}(a.default.Component),l=d;t.default=l}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-eebb70131782765d8fd6.js.map
--- public/component---src-pages-contact-thanks-js-52cc785c9a3b616fa9d3.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdesign_code_tips_v2=self.webpackChunkdesign_code_tips_v2||[]).push([[561],{6349:function(e,n,t){t.r(n);var s=t(7294),a=t(17);n.default=function(){return s.createElement(a.Z,null,s.createElement("section",{className:"section"},s.createElement("div",{className:"container"},s.createElement("div",{className:"content"},s.createElement("h1",null,"Thank you!"),s.createElement("p",null,"This is a custom thank you page for form submissions")))))}}}]);
-//# sourceMappingURL=component---src-pages-contact-thanks-js-52cc785c9a3b616fa9d3.js.map
